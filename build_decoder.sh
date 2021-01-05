75e7e03328a368ed29043a6f52d6f0bd64f7ffa0
Lawrence <1934378145@qq.com>
2021-01-05 12:31:30 +0800

    修改编译选项

--- build_decoder.sh
+++ build_decoder.sh
@@ -4,9 +4,9 @@ mkdir -p dist
 cd ../ffmpeg
 echo "emconfigure"
 emconfigure ./configure --cc="emcc" --cxx="em++" --ar="emar" --ranlib="emranlib" --prefix=$(pwd)/../WasmMediaPlayer/dist --enable-cross-compile --target-os=none \
-        --arch=x86_32 --cpu=generic --enable-gpl --enable-version3 --disable-avdevice --disable-swresample --disable-postproc --disable-avfilter --disable-swscale \
+        --arch=x86_64 --cpu=generic --enable-gpl --enable-version3 --disable-avdevice --disable-swresample --disable-postproc --disable-avfilter --disable-swscale \
         --disable-programs --disable-logging --disable-everything --enable-avformat --enable-decoder=hevc --enable-decoder=h264 --enable-decoder=aac \
-        --enable-decoder=pcm_alaw --enable-decoder=pcm_mulaw --enable-decoder=g723_1 --enable-decoder=g729 \
+        --enable-decoder=pcm_alaw --enable-decoder=pcm_mulaw --disable-decoder=g723_1 --disable-decoder=g729 \
         --disable-ffplay --disable-ffprobe --disable-asm --disable-doc --disable-devices --disable-network --disable-hwaccels \
         --disable-parsers --disable-bsfs --disable-debug --enable-protocol=file --enable-demuxer=mov --enable-demuxer=flv --disable-indevs --disable-outdevs
 if [ -f "Makefile" ]; then
